4a6cd884db76fc8bc96d6f38e271deae105c26c2
Martyn Janes <martyn@obany.com>
2018-10-23 14:24:04 +0300

    Switch to UMD build of fetch polyfill

--- src/pipelineSteps/applicationFramework/aurelia.ts
+++ src/pipelineSteps/applicationFramework/aurelia.ts
@@ -44,7 +44,7 @@ export class Aurelia extends SharedAppFramework implements IApplicationFramework
         engineVariables.toggleDevDependency(["unitejs-webdriver-plugin"],
                                             mainCondition && super.condition(uniteConfiguration.e2eTestRunner, "WebdriverIO"));
 
-        engineVariables.toggleDevDependency(["babel-plugin-transform-decorators-legacy", "babel-plugin-transform-class-properties"],
+        engineVariables.toggleDevDependency(["@babel/plugin-proposal-decorators", "@babel/plugin-proposal-class-properties"],
                                             mainCondition && super.condition(uniteConfiguration.sourceLanguage, "JavaScript"));
 
         engineVariables.toggleDevDependency(["babel-eslint"], mainCondition && super.condition(uniteConfiguration.linter, "ESLint"));
@@ -64,8 +64,10 @@ export class Aurelia extends SharedAppFramework implements IApplicationFramework
 
         const babelConfiguration = engineVariables.getConfiguration<BabelConfiguration>("Babel");
         if (babelConfiguration) {
-            ArrayHelper.addRemove(babelConfiguration.plugins, "transform-decorators-legacy", mainCondition);
-            ArrayHelper.addRemove(babelConfiguration.plugins, "transform-class-properties", mainCondition);
+            ArrayHelper.addRemove(babelConfiguration.plugins, ["@babel/plugin-proposal-decorators", { legacy: true }], mainCondition,
+                                  (obj, item) => Array.isArray(item) && item.length > 0 && item[0] === obj[0]);
+            ArrayHelper.addRemove(babelConfiguration.plugins, ["@babel/plugin-proposal-class-properties", { loose: true }], mainCondition,
+                                  (obj, item) => Array.isArray(item) && item.length > 0 && item[0] === obj[0]);
         }
 
         const esLintConfiguration = engineVariables.getConfiguration<EsLintConfiguration>("ESLint");
@@ -289,7 +291,7 @@ export class Aurelia extends SharedAppFramework implements IApplicationFramework
 
         engineVariables.toggleClientPackage("whatwg-fetch", {
             name: "whatwg-fetch",
-            main: "fetch.js"
+            main: "dist/fetch.umd.js"
         },
                                             mainCondition);
 
